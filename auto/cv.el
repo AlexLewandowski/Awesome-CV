e8b47ffaa680ece23e74c7829fcf41fbfddba138
AlexLewandowski <lewandowskialexp@gmail.com>
2018-11-05 18:02:54 -0700

    misc

--- auto/cv.el
+++ auto/cv.el
@@ -5,24 +5,25 @@
                      '(("awesome-cv" "11pt" "a4paper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("nth" "super")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
-    "section/interests"
     "section/publications"
-    "section/education"
     "section/research_exp"
+    "section/interests"
+    "section/education"
     "section/work_exp"
     "section/honors"
     "awesome-cv"
     "awesome-cv11"
-    "nth")
+    "nth"
+    "tikz")
    (LaTeX-add-xcolor-definecolors
     "custom"
     "awesome"))
